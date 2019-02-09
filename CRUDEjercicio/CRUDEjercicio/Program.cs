1592e96eb242a3ce158171415be1098ecec3c04a
Mario David Pichardo Vasquez <mariopv90@gmail.com>
2019-02-09 16:08:31 -0400

    Primer commit

---
+++ CRUDEjercicio/CRUDEjercicio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUDEjercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
